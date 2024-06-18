7ba262d9e8afbd866d92e4e8990a72e8cc07d5d5
leanalvarez <leanalvarez.dev@gmail.com>
2024-06-18 17:37:57 -0300

    Initial Commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Lean on 18/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
